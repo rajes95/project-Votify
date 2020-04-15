3ff0df9056db324b2899a17c38e74b094e4f64d6
rajes95 <sakhamuru.r@husky.neu.edu>
2020-04-15 12:08:18 -0700

    fixed bug where extra news articles will display when switching to a tab that shows fewer articles

--- src/src/votify/NewsController.java
+++ src/src/votify/NewsController.java
@@ -178,11 +178,16 @@ public class NewsController implements Initializable {
         {
             if (currentJSON.getImgURL(i) != null)
             {
+                newsLabels[i].setVisible(true);
                 newsButtons[i].setImage(new Image(currentJSON.getImgURL(i)));
                 newsLabels[i].setText(currentJSON.getSource(i)+": "+currentJSON.getTitle(i));
                 newsLabels[i].setWrapText(true);
                 newsLabels[i].setContentDisplay(ContentDisplay.TOP);
                 newsLabels[i].setGraphic(newsButtons[i]);
+            }
+            else
+            {
+                newsLabels[i].setVisible(false);
             }
         }
     }
@@ -252,181 +257,182 @@ public class NewsController implements Initializable {
     }
 
     public void openLabel00(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(0) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(0));
         }
     }
 
     public void openLabel01(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(1) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(1));
         }
     }
 
     public void openLabel02(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(2) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(2));
         }
     }
 
     public void openLabel03(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(3) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(3));
         }
     }
 
     public void openLabel04(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(4) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(4));
         }
     }
 
     public void openLabel05(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
+
         if (currentJSON.getImgURL(5) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(5));
         }
     }
 
     public void openLabel06(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(6) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(6));
         }
     }
 
     public void openLabel07(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(7) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(7));
         }
     }
 
     public void openLabel08(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(8) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(8));
         }
     }
 
     public void openLabel09(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(9) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(9));
         }
     }
 
     public void openLabel10(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(10) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(10));
         }
     }
 
     public void openLabel11(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(11) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(11));
         }
     }
 
     public void openLabel12(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(12) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(12));
         }
     }
 
     public void openLabel13(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(13) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(13));
         }
     }
 
     public void openLabel14(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(14) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(14));
         }
     }
 
     public void openLabel15(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(15) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(15));
         }
     }
 
     public void openLabel16(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(16) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(16));
         }
     }
 
     public void openLabel17(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(17) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(17));
         }
     }
 
     public void openLabel18(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(18) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(18));
         }
     }
 
     public void openLabel19(MouseEvent mouseEvent) {
-        newsWebView.setVisible(true);
-        WebEngine engine = newsWebView.getEngine();
         if (currentJSON.getImgURL(19) != null)
         {
+            newsWebView.setVisible(true);
+            WebEngine engine = newsWebView.getEngine();
             engine.load(currentJSON.getURL(19));
         }
     }
